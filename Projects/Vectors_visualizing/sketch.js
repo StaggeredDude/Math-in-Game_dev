567fff787ddf0747df8b90d2b6f540dcbe64d14d
StaggeredCheetah <som1lovessmth@gmail.com>
2026-05-05 16:09:26 +0100

    Implement drawVectors function for user input
    Added drawVectors function to update vector values from input.

--- Projects/Vectors_visualizing/sketch.js
+++ Projects/Vectors_visualizing/sketch.js
@@ -5,12 +5,15 @@ let vx1 = 50
 let vy1 = -50
 let vx2 = 100
 let vy2 = -25
+
 function draw() {  
   background(220);
   stroke(0,200,0), line(200, 200, 200 + vx1, 200 + vy1)
   stroke(200,0,0), line(200, 200, 200 + vx2, 200 + vy2)
   stroke(0,0,200), line(200, 200, 200 + vx1+vx2, 200 + vy1+vy2)
 }
+
+
 function drawVectors() {
   vx1 = parseFloat(document.getElementById("vx1").value) || 50;
   vy1 = parseFloat(document.getElementById("vy1").value) || -50;
