190ea6bbafd48fc343ef783d4d6cd7aa9430709e
StaggeredCheetah <som1lovessmth@gmail.com>
2026-05-05 15:59:20 +0100

    Implement drawVectors function for vector input
    Added drawVectors function to retrieve vector values from input fields.

--- Projects/Vectors_visualizing/sketch.js
+++ Projects/Vectors_visualizing/sketch.js
@@ -11,3 +11,9 @@ function draw() {
   stroke(200,0,0), line(200, 200, 200 + vx2, 200 + vy2)
   stroke(0,0,200), line(200, 200, 200 + vx1+vx2, 200 + vy1+vy2)
 }
+function drawVectors() {
+  vx1 = parseFloat(document.getElementById("vx1").value) || 50;
+  vy1 = parseFloat(document.getElementById("vy1").value) || -50;
+  vx2 = parseFloat(document.getElementById("vx2").value) || 100;
+  vy2 = parseFloat(document.getElementById("vy2").value) || -25;
+}
